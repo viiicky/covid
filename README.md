c983a97a6c62eb5a4848c4951fea3be43394c07b
viiicky <vikasprasad.prasad@gmail.com>
2021-05-14 23:45:08 +0530

    Make new ditrict support addition easy. Also, add Bangalore Urban district.

--- README.md
+++ README.md
@@ -2,17 +2,21 @@
 None of the countless services I subscribed to managed to send me an alert in "real-time". Honestly, didn't receive even one alert from any of these services - forget real-time. This led me to write it on my own. This script notifies users via Telegram channels about availability of Covid vaccine slots **in** real-time.
 
 ## Supported notifications
-Use Telegram to search for channel(s) of your interest from the below table:  
+Use Telegram to search and join channel(s) of your interest from the table below:  
 
-| District | Minimum age limit | Vaccine name | Telegram channel |
-| --- | --- | --- | --- |
-| Gwalior | 45 | Covaxin | @gwl_covaxin_45 |
-| Gwalior | 18 | Covishield | @gwl_covishield_18 |
-| Gwalior | 45 | Covishield | @gwl_covishield_45 |
+| District | Vaccine name | Minimum age limit | Telegram channel | Telegram channel status |
+| --- | --- | :---: | --- | :---: |
+| Gwalior | Covaxin | 45 | @gwl_covaxin_45 | 🟢 |
+| Gwalior | Covishield | 18 | @gwl_covishield_18 | 🟢 |
+| Gwalior | Covishield | 45 | @gwl_covishield_45 | 🟢 |
+| Bangalore Urban | Covaxin | 18 | @blr_urban_covaxin_18 | 🟢 |
+| Bangalore Urban | Covaxin | 45 | @blr_urban_covaxin_45 | 🟢 |
+| Bangalore Urban | Covishield | 18 | @blr_urban_covishield_18 | 🟢 |
+| Bangalore Urban | Covishield | 45 | @blr_urban_covishield_45 | 🟢 |
 
 ## Remarks
-- If you wish to receive alerts for a different district, feel free to raise an issue, and I will add support for it. Or, if you can, you could just submit a pull request.
+- If you wish to receive alerts for a different district, feel free to raise an issue, and I will add support for it.
 
 - The script needs to pass `User-Agent` header to the co-vin api, and it seems they have put up cache also. Because of these factors, some of the alerts might not match the info that you see on the portal. We can't do much about these rare cases. If you really want to minimise this, you can always run this code on your own and supply the relevant `User-Agent` value.
 
-- When slots are available, these channels would see a surge of alerts. Feel free to mute the channel for a given duration to avoid being disturbed if needed.
+- When slots are available, channels would see a surge of alerts. Feel free to mute the channel for a given duration to avoid being disturbed if needed.
--- vaccinate.py
+++ vaccinate.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from math import ceil
 from time import sleep, perf_counter
 
@@ -13,10 +14,9 @@ load_dotenv()
 
 
 class Center:
-    def __init__(self, center_id, name, address, sessions):
+    def __init__(self, center_id, name, sessions):
         self.center_id = center_id
         self.name = name
-        self.address = address
         self.sessions = sessions
 
     def __eq__(self, o: 'Center') -> bool:
@@ -27,81 +27,89 @@ class Center:
 
 
 async def send_telegram(chat_id, message, session):
-    if message:
+    if message and chat_id:
+        # print(f'channel: {chat_id}; centers:\n{message}')
         url = f"https://api.telegram.org/bot{os.environ['TELEGRAM_BOT_TOKEN']}/sendMessage?chat_id={chat_id}" \
               f"&parse_mode=HTML&text=<pre>{message}</pre>"
         async with session.post(url):
             pass
 
 
-async def send_notifications(cova_18_message, cova_45_message, covi_18_message, covi_45_message):
+async def send_notifications(chat_ids, cova_18_message, cova_45_message, covi_18_message, covi_45_message):
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(
-            send_telegram(os.environ['TELEGRAM_CHAT_ID_COVA_18'], cova_18_message, session),
-            send_telegram(os.environ['TELEGRAM_CHAT_ID_COVA_45'], cova_45_message, session),
-            send_telegram(os.environ['TELEGRAM_CHAT_ID_COVI_18'], covi_18_message, session),
-            send_telegram(os.environ['TELEGRAM_CHAT_ID_COVI_45'], covi_45_message, session)
+            send_telegram(chat_ids.get('cova_18'), cova_18_message, session),
+            send_telegram(chat_ids.get('cova_45'), cova_45_message, session),
+            send_telegram(chat_ids.get('covi_18'), covi_18_message, session),
+            send_telegram(chat_ids.get('covi_45'), covi_45_message, session)
         )
 
 
-def get_calendar(district, search_date):
+def get_calendar(district_id, search_date):
     response = requests.get(f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
-                            f'?district_id={district}&date={search_date}', headers={'User-Agent': 'Mozilla/5.0 ('
-                                                                                                  'Macintosh; Intel '
-                                                                                                  'Mac OS X 10_15_7) '
-                                                                                                  'AppleWebKit/537.36 '
-                                                                                                  '(KHTML, '
-                                                                                                  'like Gecko) '
-                                                                                                  'Chrome/90.0.4430'
-                                                                                                  '.93 Safari/537.36'})
+                            f'?district_id={district_id}&date={search_date}', headers={'User-Agent': 'Mozilla/5.0 ('
+                                                                                                     'Macintosh; '
+                                                                                                     'Intel Mac OS X '
+                                                                                                     '10_15_7) '
+                                                                                                     'AppleWebKit/537'
+                                                                                                     '.36 (KHTML, '
+                                                                                                     'like Gecko) '
+                                                                                                     'Chrome/90.0'
+                                                                                                     '.4430.93 '
+                                                                                                     'Safari/537.36'})
     response.raise_for_status()
     return response.json()
 
 
-def get_hospitals(district, search_date):
+def get_hospitals(district_id, search_date):
     covaxin_18_hospitals = set()
     covaxin_45_hospitals = set()
     covishield_18_hospitals = set()
     covishield_45_hospitals = set()
 
-    centers = get_calendar(district, search_date)['centers']
+    centers = get_calendar(district_id, search_date)['centers']
     for center in centers:
         for session in center['sessions']:
             if datetime.strptime(session['date'], '%d-%m-%Y').date() >= date.today() and session['available_capacity']:
                 vaccine = session['vaccine'].strip().upper()
-                center = Center(center['center_id'], center['name'], center['address'], center['sessions'])
+                new_center = Center(center['center_id'], center['name'], center['sessions'])
                 if vaccine == 'COVAXIN':
                     if session['min_age_limit'] == 18:
-                        covaxin_18_hospitals.add(center)
+                        covaxin_18_hospitals.add(new_center)
                     elif session['min_age_limit'] == 45:
-                        covaxin_45_hospitals.add(center)
+                        covaxin_45_hospitals.add(new_center)
                 elif vaccine == 'COVISHIELD':
                     if session['min_age_limit'] == 18:
-                        covishield_18_hospitals.add(center)
+                        covishield_18_hospitals.add(new_center)
                     elif session['min_age_limit'] == 45:
-                        covishield_45_hospitals.add(center)
+                        covishield_45_hospitals.add(new_center)
 
     return covaxin_18_hospitals, covaxin_45_hospitals, covishield_18_hospitals, covishield_45_hospitals
 
 
 if __name__ == "__main__":
+    districts = json.loads(os.environ['DISTRICTS'])
     while True:
         start = perf_counter()
-        # find centers
-        cova_18, cova_45, covi_18, covi_45 = get_hospitals(os.environ['DISTRICT_ID'], date.today().strftime('%d-%m-%Y'))
+        for district in districts:
+            # find centers
+            cova_18, cova_45, covi_18, covi_45 = get_hospitals(district['district_id'],
+                                                               date.today().strftime('%d-%m-%Y'))
 
 
-        def format_hospital(hospital):
-            return {'name': hospital.name, 'address': hospital.address,
-                    'available_capacity': [ceil(s['available_capacity']) for s in hospital.sessions]}
+            def format_hospital(hospital):
+                return {'name': hospital.name,
+                        'available_capacity': [ceil(s['available_capacity']) for s in hospital.sessions]}
 
-        # prepare output
-        cova_18_output = tabulate([format_hospital(h) for h in cova_18])
-        cova_45_output = tabulate([format_hospital(h) for h in cova_45])
-        covi_18_output = tabulate([format_hospital(h) for h in covi_18])
-        covi_45_output = tabulate([format_hospital(h) for h in covi_45])
 
-        # send notifications
-        asyncio.run(send_notifications(cova_18_output, cova_45_output, covi_18_output, covi_45_output))
+            # prepare output
+            cova_18_output = tabulate([format_hospital(h) for h in cova_18])
+            cova_45_output = tabulate([format_hospital(h) for h in cova_45])
+            covi_18_output = tabulate([format_hospital(h) for h in covi_18])
+            covi_45_output = tabulate([format_hospital(h) for h in covi_45])
+
+            # send notifications
+            asyncio.run(send_notifications(district, cova_18_output, cova_45_output, covi_18_output, covi_45_output))
+
         print(f'time taken: {perf_counter() - start} seconds')
         sleep(int(os.environ['SLEEP_SECONDS']))
